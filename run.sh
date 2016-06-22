c3ae96f386d3c884c15076204e079f7a3939e255
Layon <layon1993@gmail.com>
2016-06-22 14:27:11 +0100

    fix benchmark script

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -e
-
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 rubies=( "2.1.7" "2.1.8" "2.1.9" "2.2.0" "2.2.1" "2.2.2" "2.2.3" "2.2.4" "2.3.0" "2.4.0-preview1")
+# rubies=( "2.1.7"  "2.2.1"  "2.3.0" "2.4.0-preview1")
 
 for i in "${rubies[@]}"
 do
   echo "====================================================="
   echo "$i: benchmarking"
   echo "====================================================="
+
   rvm use $i
+  echo `ruby -v`
   ruby benchmark.rb
 done
